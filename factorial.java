a237f1773b7050676a3ae4bc3a2ab2fee83ca4b0
System 05 <system05pentium@gmail.com>
2024-11-26 13:15:59 +0530

    Factorial Code added

--- factorial.java
+++ factorial.java
@@ -2,29 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class largest{
+class factorial{
     public static void main(String ar[]){
-        new LargestCheckFrame();
+        new factorialCalc();
     }
 }
 
-class LargestCheckFrame extends JFrame implements ActionListener{
-    JTextArea A1,A2,A3;
+class factorialCalc extends JFrame implements ActionListener{
+    JTextArea A1,A3;
     JButton B1;
-    LargestCheckFrame(){
+    factorialCalc(){
         setLayout(null);
         setSize(400,400);
         A1=new JTextArea();
         A1.setBounds(10,10,100,20);
-        A2=new JTextArea();
-        A2.setBounds(120,10,100,20);
         A3=new JTextArea();
         A3.setBounds(10,100,300,20);
-        B1=new JButton("Check");
+        B1=new JButton("Submit");
         B1.setBounds(10,50,100,30);
         B1.addActionListener(this);
         add(A1);
-        add(A2);
         add(A3);
         add(B1);
         setVisible(true);
@@ -32,12 +29,11 @@ class LargestCheckFrame extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent ae){
         try{
             int a= Integer.parseInt(A1.getText());
-            int b= Integer.parseInt(A2.getText());
-            if(a>b){
-                A3.setText(a+" is larger than "+b);
-            }else{
-                A3.setText(b+" is larger than "+a);
+            int fact=1;
+            for(int i=1;i<=a;i++){
+                fact*=i;
             }
+            A3.setText("Factorial of "+a+" is "+fact);
         }catch(Exception e){
             A3.setText("Error");
         }
